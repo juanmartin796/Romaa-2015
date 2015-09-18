8bcc34a29974ecd43322bc20e86bf027c852d8ef
Juan Martin Fernandez <juanmartin_796@hotmail.com>
2015-09-18 12:13:05 -0300

    Primera version del Roma. Es la 1.2 desde que se empezo a usar git

---
+++ Romaa-2015.pro
@@ -0,0 +1,29 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-06-22T22:19:23
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+TARGET = Romaa-2015
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
+
+#-------------------Librerías---------------------
+INCLUDEPATH += "/usr/include/opencv"
+INCLUDEPATH += "/usr/local/include/player-3.0"
+LIBS += `pkg-config opencv --cflags --libs`
+LIBS += -L/usr/local/lib64 -lplayerc++
+
+CONFIG += extserialport
+#-------------------------------------------------
+
+ QT += widgets
---
+++ mainwindow.cpp
@@ -0,0 +1,192 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+//#include "cv.h" // incluye las definiciones de OpenCV
+#include <opencv/cv.h>
+//#include "cvaux.h" // Stuff.
+#include <opencv/cvaux.h>
+//#include "highgui.h" // incluye highGUI. Necesario para crear ventanas de visualizacion
+#include <opencv/highgui.h>
+#include "iostream"
+#include "qextserialport.h"
+
+#include <libplayerc++/playerc++.h>
+#include <unistd.h>
+#include<qprocess.h>
+
+using namespace std;
+bool prendido=false;
+
+QextSerialPort *port;
+
+void MainWindow::onDataAvailable(){
+    cout << "Evento!!" << endl;
+    QByteArray data = port->readAll();
+    if(data.length() > 0){
+        cout << "Recibido: " << endl;
+        for(int i = 0; i < data.length(); i++){
+            cout << data.at(i) << endl;
+        }
+        cout << "Fin recibido" << endl;
+    }
+}
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    /*QProcess process;
+    process.start("player", QStringList() << "/home/juanmartin/Dropbox/Proyectos GUDA/Romaa-2015/Stage/vacio.cfg");
+    sleep(3);*/
+
+
+    port = new QextSerialPort("/dev/ttyACM0");
+    port->setBaudRate(BAUD9600);
+    port->setQueryMode(QextSerialPort::EventDriven);
+    port->open(QIODevice::ReadWrite);
+    port->flush();
+    //port->putChar('a');
+    connect(port, SIGNAL(readyRead()), this, SLOT(onDataAvailable()));
+
+    //**********************************************************************************
+    //          Codigo para hacer mover al romaa para adelante
+    //**********************************************************************************
+    using namespace PlayerCc;
+
+    PlayerClient    robot("localhost", 6665);
+    Position2dProxy p2dProxy(&robot,0);
+    p2dProxy.SetMotorEnable(1);
+    //p2dProxy.SetSpeed(0.5, 0);
+    //sleep(5); //va a andar por 5 segundos;
+
+
+    //*********************************************************************************
+    //          Codigo  del filtro de particulas detectando el color rojo en el sig IF:
+    //          if ((int)s.val[0]<=80 && (int)s.val[1]<=80 && (int)s.val[2]>=160){
+    //***********************************************************************************
+
+
+    CvCapture *capture = NULL;
+    capture = cvCreateCameraCapture(1);
+
+    if(!capture){
+        cout<<"error"<<endl;
+    }
+
+    IplImage* frame;
+
+    frame = NULL;
+    //IplImage* gray_frame = NULL ;
+
+    //cvNamedWindow( "Centro de masa", CV_WINDOW_AUTOSIZE );
+    cvNamedWindow( "Ventana del frame", CV_WINDOW_FREERATIO );
+
+    int x,y,i,j;
+    int detecta=0;
+    int punto =0;
+    int puntoAzul =0;
+    CvScalar s;
+
+    int sumPosX, sumPosY, sumPosObstX, sumPosObstY;
+    double posRepresX, posRepresY, posRepresObstX, posRepresObstY;
+    bool obstaculo;
+
+    while(1) {
+        frame = cvQueryFrame( capture );//Grabs the frame from a file
+        //cvCircle(frame, cvPoint(200,200),100, CV_RGB(0,0,255), CV_FILLED, CV_AA,0);
+        if( !frame ) break;
+
+        cvFlip(frame, frame, 1);
+
+        sumPosX=0;
+        sumPosY=0;
+        sumPosObstX=0;
+        sumPosObstY=0;
+        while (true){
+            x= rand() % (frame->width-25);
+            y= rand() % (frame->height-25);
+
+            detecta++;
+            if (x>=1 && y>=1){
+                for (i=-1; i<=1; i++){
+                    for (j=-1; j<=1; j++){
+                        s= cvGet2D(frame, y-j, x-i);
+                        //detecta el color rojo
+                        if ((int)s.val[0]<=80 && (int)s.val[1]<=80 && (int)s.val[2]>=160){ //colores en orden BGR
+                            cvCircle(frame, cvPoint(x-i,y-j),10, CV_RGB(0,255,255), CV_FILLED, CV_AA,0);
+                            sumPosX+=x-i;
+                            sumPosY+=y-j;
+                            punto++;
+                        }
+                        //Detecta el color azul
+                        if ((int)s.val[0]>=160 && (int)s.val[1]<=80 && (int)s.val[2]<=80){ //colores en orden BGR
+                            cvCircle(frame, cvPoint(x-i,y-j),10, CV_RGB(255,0,255), CV_FILLED, CV_AA,0);
+                            sumPosObstX+=x-i;
+                            sumPosObstY+=y-j;
+                            puntoAzul++;
+                        }
+                    }
+                }
+            }
+            if (punto==10) {break;}
+            if (detecta==10000) {break;}
+        }
+
+        cvCircle(frame, cvPoint(frame->width/2-30,240),10, CV_RGB(255,255,255), CV_FILLED, CV_AA,0); //puntos de referencia centro vertical
+        cvCircle(frame, cvPoint(frame->width/2+30,240),10, CV_RGB(255,255,255), CV_FILLED, CV_AA,0); //puntos de referencia centro vertical
+
+        //*************************** Se mueve cuando es detectado por la camara         *******************************************
+        //*************************** Hace que vaya siempre por el medio vertical de la pantalla. *********************************************
+
+        if (punto!=0 && obstaculo==false){
+            posRepresX= sumPosX/punto;
+            posRepresY= sumPosY/punto;
+            cvCircle(frame, cvPoint(posRepresX,posRepresY),10, CV_RGB(255,255,0), CV_FILLED, CV_AA,0);
+            if (posRepresX <= frame->width/2-30){
+                p2dProxy.SetSpeed(0.5, DTOR(-20)); //20 grados convertidos en radianes
+            } else if (posRepresX >= frame->width/2+30){
+                p2dProxy.SetSpeed(0.5, DTOR(20)); //20 grados convertidos en radianes
+            } else {
+                p2dProxy.SetSpeed(0.5, 0);
+            }
+            prendido=true;
+        }
+
+        //****************************Si encuentra un obstaculo a una distancia menor a 200 px se detiene ******************************
+        if (puntoAzul!=0){
+            posRepresObstY= sumPosObstY/puntoAzul;
+            if (puntoAzul>5 && punto!=0 && posRepresY- posRepresObstY<=200){
+                obstaculo=true;
+            } else {
+                obstaculo= false;
+            }
+        }else{
+            obstaculo=false;
+        }
+
+        //*************************************** Si no se detecta el robot, hace que pare, y no se mueva ********************************************************
+
+        if ( ( detecta==10000 && prendido==true ) || obstaculo==true ){
+            p2dProxy.SetSpeed(0,0);
+            //port->putChar('c');
+            prendido=false;
+        }
+        //*************************************************************************************************************
+
+        punto=0;
+        puntoAzul=0;
+        detecta=0;
+
+        cvShowImage("Ventana del frame", frame);
+
+        char c=cvWaitKey(33);
+        if( c == 27 ) break;
+    }
+
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
